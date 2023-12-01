4e8d18ed0203c1f6feeaa1eab8d5168b458e5022
fargito <fr.farge96@gmail.com>
2023-12-01 16:01:47 +0100

    feat: day1 part1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -8,11 +8,16 @@ async fn fake_error() -> StatusCode {
     StatusCode::INTERNAL_SERVER_ERROR
 }
 
+async fn recalibrate(Path((num1, num2)): Path<(u64, u64)>) -> impl IntoResponse {
+    (num1 ^ num2).pow(3).to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/-1/error", get(fake_error));
+        .route("/-1/error", get(fake_error))
+        .route("/1/:num1/:num2", get(recalibrate));
 
     Ok(router.into())
 }
